4a5e08ce140a57bd2bf5abd1d5b4744062457978
KKETKAEW <kketkaew111@gmail.com>
2024-12-09 14:21:15 +0700

    LabSheet03Homework

---
+++ LabSheet03Homework/src/Lab303_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Lab303_1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Input a sentense : ");
+		String sentense = scan.nextLine();
+
+		
+		scan.close();
+	}
+
+}
