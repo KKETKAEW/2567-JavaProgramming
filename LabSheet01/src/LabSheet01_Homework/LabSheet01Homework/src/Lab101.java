6e54168fa2a15be4f2053d9264f9003e097bc7a8
KKETKAEW <kketkaew111@gmail.com>
2024-12-09 13:26:00 +0700

    LabSheet01Homewrk

---
+++ LabSheet01/src/LabSheet01_Homework/LabSheet01Homework/src/Lab101.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.text.*;
+ 
+public class Lab101 {
+ 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input Product Name	: ");
+		String nameofProduct = scan.nextLine();
+		System.out.print("Input Product Unit	: ");
+		int unitofProduct = scan.nextInt();
+		System.out.print("Input price per unit 	: ");
+		double priceofProduct = scan.nextDouble();
+		System.out.println("------------------------------------------------");
+		double total = priceofProduct*unitofProduct;
+		DecimalFormat frm = new DecimalFormat("#,###.00");
+		System.out.println("Total Price is "+(frm.format(total))+" baht.");
+		System.out.println("------------------------------------------------");
+		System.out.print("How many discount (%) 	: ");
+		double dicountProduct = scan.nextDouble();
+		System.out.println("------------------------------------------------");
+		double discount = (total*dicountProduct)/100;
+		double totalPrice = total-discount;
+		System.out.println("Discount from 15% 	  "+frm.format(discount)+" baht.");
+		System.out.println("Amount to be paid 	  "+frm.format(totalPrice)+" baht.");
+		scan.close();
+	}
+ 
+}
---
+++ LabSheet01/src/LabSheet01_Homework/LabSheet01Homework/src/Lab102.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ 
+public class Lab102 {
+ 
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input the number of minutes: ");
+		int minute =scan.nextInt();
+		int minuteofOneyear = 60*24*365;
+		int minuteofOneday = 60*24;
+		int year = minute/minuteofOneyear;
+		int minuteMod = minute%minuteofOneyear;
+		int day = minuteMod/minuteofOneday;
+		System.out.println(minute+" minutes is approximately "+year+" years and "+day+" days");
+		scan.close();
+ 
+	}
+ 
+}
