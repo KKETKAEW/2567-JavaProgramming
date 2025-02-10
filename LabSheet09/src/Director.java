d438eedceb0af8d3e258bbbd4376f649dcd847eb
KKETKAEW <kketkaew111@gmail.com>
2025-02-10 14:06:30 +0700

    LabSheet09Homework

---
+++ LabSheet09/src/Director.java
@@ -0,0 +1,39 @@
+
+public class Director {
+	private String name;
+	private String email;
+	private char gender;
+	
+	public Director(String name, String email, char gender) {
+		this.name= name;
+		this.email=email;
+		this.gender=gender;
+	}
+	
+	public Director(String name, String email) {
+		this(name,email,' ');
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getGenderName() {
+		if(gender=='m'||gender=='M') {
+			return "male";
+		}else if(gender=='f'||gender=='F') {
+			return "Female";
+		}else {
+			return null;
+		}
+	}
+	
+	public String toString(){
+		return name + " (" +email+ ";" + getGenderName() + ")";
+	}
+
+}
